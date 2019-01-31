ea4da0a51e22032bbd8ba87590d77b2bdb7bd19c
Peter Jedinger <peter1.jedinger@gmail.com>
2019-01-31 11:54:12 +0100

    #3 Design Update, Info Screen, Start Screen, Icon
    Added new App Icon
    New Colors, UI Elements
    Added Info Screen

---
+++ CellularSignalScanner/app/src/main/java/fh_ooe/at/cellularsignalscanner/HomeActivity.java
@@ -0,0 +1,49 @@
+package fh_ooe.at.cellularsignalscanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        final Button startScanButton = findViewById(R.id.home_startscan_button);
+        Button historyButton = findViewById(R.id.home_history_button);
+
+        startScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater =getMenuInflater();
+        menuInflater.inflate(R.menu.home_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //only one element
+        startInfoScreen();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void startInfoScreen(){
+        Intent i = new Intent(this, InfoActivity.class);
+        startActivity(i);
+    }
+}
---
+++ CellularSignalScanner/app/src/main/java/fh_ooe/at/cellularsignalscanner/InfoActivity.java
@@ -0,0 +1,27 @@
+package fh_ooe.at.cellularsignalscanner;
+
+import android.support.v4.app.SupportActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        this.finish();
+        return super.onOptionsItemSelected(item);
+    }
+}
